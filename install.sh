a054656102a86ab70d7b5be210e89f2d62db3b79
Jason Theiler <turret.jason@outlook.com>
2020-09-01 02:31:52 +0200

    Update install scripts
    - Update configuration files

--- install.sh
+++ install.sh
@@ -11,15 +11,15 @@ trap 'stty echo' EXIT
 stty -echo
 
 # Gets terminal-dependent values of common text styling options.
-BOLD="$(tput bold 2>/dev/null || printf '')"
-BLACK="$(tput setaf 0 2>/dev/null || printf '')"
-RED="$(tput setaf 1 2>/dev/null || printf '')"
-GREEN="$(tput setaf 2 2>/dev/null || printf '')"
-YELLOW="$(tput setaf 3 2>/dev/null || printf '')"
-BLUE="$(tput setaf 4 2>/dev/null || printf '')"
-PURPLE="$(tput setaf 5 2>/dev/null || printf '')"
-CYAN="$(tput setaf 6 2>/dev/null || printf '')"
-RESET="$(tput sgr0 2>/dev/null || printf '')"
+BOLD="$(tput bold 2>/dev/null || printf "")"
+BLACK="$(tput setaf 0 2>/dev/null || printf "")"
+RED="$(tput setaf 1 2>/dev/null || printf "")"
+GREEN="$(tput setaf 2 2>/dev/null || printf "")"
+YELLOW="$(tput setaf 3 2>/dev/null || printf "")"
+BLUE="$(tput setaf 4 2>/dev/null || printf "")"
+PURPLE="$(tput setaf 5 2>/dev/null || printf "")"
+CYAN="$(tput setaf 6 2>/dev/null || printf "")"
+RESET="$(tput sgr0 2>/dev/null || printf "")"
 
 # Prints an error message.
 print_error() {
