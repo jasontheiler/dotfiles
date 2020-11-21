56a91796ba58168329b46c4108dd5c01a3197c24
Jason Theiler <turret.jason@outlook.com>
2020-11-21 15:59:16 +0100

    Fix antibody not being found during portable install

--- antibody/install.sh
+++ antibody/install.sh
@@ -3,13 +3,12 @@
 # Exits immediately if an error occurs.
 set -e
 
-# Installs Antibody.
+# Installs Antibody and bundles plugins.
 # See: https://getantibody.github.io/
 if [ "$PORTABLE" != true ]; then
   curl -fsSL https://git.io/antibody | sudo sh -s -- -b "/usr/local/bin"
+  antibody bundle < "${DOTFILES}/antibody/bundle.txt" > "${HOME}/.zsh_plugins"
 else
   curl -fsSL https://git.io/antibody | sh -s -- -b "${HOME}/.local/bin"
+  "${HOME}/.local/bin/antibody" bundle < "${DOTFILES}/antibody/bundle.txt" > "${HOME}/.zsh_plugins"
 fi
-
-# Bundles plugins.
-antibody bundle < "${DOTFILES}/antibody/bundle.txt" > "${HOME}/.zsh_plugins"
