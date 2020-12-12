a90954fd633fdae7d9f8f6715d1298b183177ae1
Jason Theiler <turret.jason@outlook.com>
2020-12-12 16:21:38 +0100

    Update fish colors

--- fish/conf.d/colors.fish
+++ fish/conf.d/colors.fish
@@ -6,11 +6,15 @@ set fish_color_comment --dim normal
 set fish_color_end magenta
 set fish_color_error red
 set fish_color_escape magenta
-set fish_color_match --background=blue
+set fish_color_match --reverse
 set fish_color_normal normal
 set fish_color_operator magenta
 set fish_color_param cyan
 set fish_color_quote yellow
 set fish_color_redirection cyan
-set fish_color_search_match --background=blue
+set fish_color_search_match --reverse
 set fish_color_valid_path --underline
+
+# Sets colors for fish's completion pager.
+set fish_pager_color_description yellow
+set fish_pager_color_progress --reverse blue
