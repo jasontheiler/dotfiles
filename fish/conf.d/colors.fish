487ac856a2bb90d9301bf85489ead3ac3faaef72
Jason Theiler <turret.jason@outlook.com>
2020-12-16 01:11:58 +0100

    Update fish colors

--- fish/conf.d/colors.fish
+++ fish/conf.d/colors.fish
@@ -2,17 +2,17 @@
 # See: https://fishshell.com/docs/current/index.html#variables-for-changing-highlighting-colors
 set fish_color_autosuggestion brblack
 set fish_color_cancel red
-set fish_color_command normal
+set fish_color_command cyan
 set fish_color_comment brblack
-set fish_color_end magenta
+set fish_color_end normal
 set fish_color_error red
 set fish_color_escape magenta
 set fish_color_match --reverse
 set fish_color_normal normal
 set fish_color_operator magenta
-set fish_color_param cyan
+set fish_color_param normal
 set fish_color_quote yellow
-set fish_color_redirection cyan
+set fish_color_redirection magenta
 set fish_color_search_match --reverse
 set fish_color_valid_path --underline
 
