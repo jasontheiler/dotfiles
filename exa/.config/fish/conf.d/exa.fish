e48173b17af5f1b023da85b4451e7969d0c186c0
Jason Theiler <turret.jason@outlook.com>
2021-05-08 17:14:52 +0200

    Update exa abbreviations
    - Update Starship config

--- exa/.config/fish/conf.d/exa.fish
+++ exa/.config/fish/conf.d/exa.fish
@@ -3,12 +3,10 @@
 set -x EXA_COLORS "xx=1;30"
 
 # Adds exa abbreviations.
-abbr -a l exa -F
-abbr -a la exa -Fa
-abbr -a ll exa -Fl --git
-abbr -a lla exa -Fla --git
-
-# Adds exa --tree -L abbreviations.
+abbr -a l exa -F --tree -L 1
+abbr -a la exa -Fa --tree -L 1
+abbr -a ll exa -Fl --git --tree -L 1
+abbr -a lla exa -Fla --git --tree -L 1
 abbr -a t exa -F --tree -L
 abbr -a ta exa -Fa --tree -L
 abbr -a tl exa -Fl --git --tree -L
