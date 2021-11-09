5d43539f9b155f66998bf3f7f0bea242f8ad28d5
Jason Theiler <turret.jason@outlook.com>
2021-11-09 15:54:33 +0100

    feat: add type sorting to exa abbreviations

--- exa/.config/fish/conf.d/exa.fish
+++ exa/.config/fish/conf.d/exa.fish
@@ -3,11 +3,11 @@
 set -x EXA_COLORS "xx=1;30"
 
 # Adds exa abbreviations.
-abbr -a l exa -F --tree -L 1
-abbr -a la exa -Fa --tree -L 1
-abbr -a ll exa -Fl --git --tree -L 1
-abbr -a lla exa -Fla --git --tree -L 1
-abbr -a t exa -F --tree -L
-abbr -a ta exa -Fa --tree -L
-abbr -a tl exa -Fl --git --tree -L
-abbr -a tla exa -Fla --git --tree -L
+abbr -a l exa -Fs type --tree -L 1
+abbr -a la exa -Fas type --tree -L 1
+abbr -a ll exa -Fls type --git --tree -L 1
+abbr -a lla exa -Flas type --git --tree -L 1
+abbr -a t exa -Fs type --tree -L
+abbr -a ta exa -Fas type --tree -L
+abbr -a tl exa -Fls type --git --tree -L
+abbr -a tla exa -Flas type --git --tree -L
