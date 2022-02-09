6f337be174f0298c5b32fee6066fd8287ae389ca
Jason Theiler <turret.jason@outlook.com>
2022-02-09 15:13:12 +0100

    feat: change Git specific config behaviour
    - feat: remove Windows Git credential helper config (use GitHub CLI instead)

--- git/.config/fish/functions/git_setup.fish
+++ git/.config/fish/functions/git_setup.fish
@@ -21,47 +21,43 @@ end
 # Sets up the user's Git configuration.
 function git_setup -d "Sets up the user's Git configuration."
     if contains -- -g $argv || [ ! -d ./.git/ ]
-        set location global
-        set git_config_location --file "$HOME/.gitconfig_user"
+        set scope global
+        set file --global
     else
-        set location local
+        set scope local
+        set file --local
     end
 
-    # Sets the Windows Git credential helper as the default Git credential helper if the current OS
-    # is a WSL2 instance, the Windows Git credential helper is installed in its default directory
-    # and none other is configured.
-    # See: https://docs.microsoft.com/windows/wsl/tutorials/wsl-git
-    if [ "$location" = global ] \
-            && [ -z (git config $git_config_location --get credential.helper) ] \
-            && [ -f "/mnt/c/Program Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe" ]
-        git config \
-            $git_config_location \
-            credential.helper \
-            "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe"
+    # Shows the user the determined scope.
+    echo -se \n (set_color yellow) "!" (set_color normal) " Scope: " (set_color -u) $scope
+
+    # Sets the path for included Git defaults.
+    if [ "$scope" = global ]
+        git config $file include.path "~/.gitconfig_defaults"
     end
 
     # Prompts the user to set a Git author email.
-    set user_email (prompt_read "What is your $location Git author email?")
+    set user_email (prompt_read "What is your Git author email?")
 
     # Sets the user's Git author email if the input is not empty.
     if [ -n "$user_email" ]
-        git config $git_config_location user.email "$user_email"
+        git config $file user.email "$user_email"
     end
 
     # Prompts the user to set a Git author name.
-    set user_name (prompt_read "What is your $location Git author name?")
+    set user_name (prompt_read "What is your Git author name?")
 
     # Sets the user's Git author name if the input is not empty.
     if [ -n "$user_name" ]
-        git config $git_config_location user.name "$user_name"
+        git config $file user.name "$user_name"
     end
 
     # Prompts the user to set a GPG signing key ID.
-    set user_signingkey (prompt_read "What is your $location GPG signing key ID?\n (This will enable GPG commit signing $location \bly.)")
+    set user_signingkey (prompt_read "What is your GPG signing key ID?\n (This will enable GPG commit signing.)")
 
     # Sets the user's GPG signing key ID and enables GPG commit signing if the input is not empty.
     if [ -n "$user_signingkey" ]
-        git config $git_config_location user.signingkey "$user_signingkey"
-        git config $git_config_location commit.gpgsign true
+        git config $file user.signingkey "$user_signingkey"
+        git config $file commit.gpgsign true
     end
 end
