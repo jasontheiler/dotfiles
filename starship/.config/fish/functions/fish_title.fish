0d137886f003015a513e114e09b2a949b7fe84eb
Jason Theiler <turret.jason@outlook.com>
2022-05-13 17:26:37 +0200

    feat: show executed command in terminal title

--- starship/.config/fish/functions/fish_title.fish
+++ starship/.config/fish/functions/fish_title.fish
@@ -1,4 +1,4 @@
-# Prints the username.
+# Prints the current username.
 function title_username
     if [ (id -u) = 0 ] \
             || [ $USER != $LOGNAME ] \
@@ -9,7 +9,7 @@ function title_username
     end
 end
 
-# Prints the hostname.
+# Prints the current hostname.
 function title_hostname
     if [ $SSH_CONNECTION ] \
             || [ $SSH_CLIENT ] \
@@ -18,7 +18,18 @@ function title_hostname
     end
 end
 
+# Prints the executed command.
+function title_command
+    if [ $argv ]
+        echo -n -s " ❯ " $argv
+    end
+end
+
 # Prints fish's title.
 function fish_title
-    echo -n -s (title_username) (title_hostname) (string replace "$HOME" "~" "$PWD")
+    echo -n -s \
+        (title_username) \
+        (title_hostname) \
+        (string replace "$HOME" "~" "$PWD") \
+        (title_command $argv)
 end
