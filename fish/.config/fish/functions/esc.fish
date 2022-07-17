01ed62bad91b1bdbebced2f645742cfe331d6713
Jason Theiler <turret.jason@outlook.com>
2022-07-17 03:19:24 +0200

    feat: finalize first iteration of esc util

--- fish/.config/fish/functions/esc.fish
+++ fish/.config/fish/functions/esc.fish
@@ -1,65 +1,48 @@
-# ...
-function esc -d "..."
-    switch $argv[1]
-        case cursor
-            switch $argv[2]
-                case up
-                    set code '\x1B[1A'
-                    set count $argv[-1]
-                case down
-                    set code '\x1B[1B'
-                    set count $argv[-1]
-                case forward
-                    set code '\x1B[1C'
-                    set count $argv[-1]
-                case backward
-                    set code '\x1B[1D'
-                    set count $argv[-1]
-                case next_line
-                    set code '\x1B[E'
-                    set count $argv[-1]
-                case prev_line
-                    set code '\x1B[F'
-                    set count $argv[-1]
-                case hide
-                    set code '\x1B[?25l'
-                case show
-                    set code '\x1B[?25h'
-                case save
-                    set code '\x1B7'
-                case load
-                    set code '\x1B8'
-            end
-        case erase
-            switch $argv[2]
-                case line
-                    set code '\x1B[2K'
-                case screen
-                    set code '\x1B[2J'
-            end
-        case scroll
-            switch $argv[2]
-                case up
-                    set code '\x1B[S'
-                    set count $argv[-1]
-                case down
-                    set code '\x1B[T'
-                    set count $argv[-1]
-            end
-    end
-
-    if test -z $code
-        return 1
-    end
+# Prints common ANSI escape codes.
+function esc -d "Prints common ANSI escape codes."
+    set count $argv[-1]
 
     if not string match -qr '^\d+$' $count
         or test $count -lt 1
         set count 1
     end
 
-    set -l i 1
-    while test $i -le $count
-        echo -sn (string unescape $code)
-        set i (math $i + 1)
+    switch $argv[1]
+        case cursor
+            switch $argv[2]
+                case up
+                    set code $count A
+                case down
+                    set code $count B
+                case forward
+                    set code $count C
+                case backward
+                    set code $count D
+                case next_line
+                    set code $count E
+                case prev_line
+                    set code $count F
+                case column
+                    set code $count G
+                case show
+                    set code '?25h'
+                case hide
+                    set code '?25l'
+            end
+        case erase
+            switch $argv[2]
+                case line
+                    set code 2K
+                case line_start
+                    set code 1K
+                case line_end
+                    set code 0K
+            end
     end
+
+    if test (count $code) -lt 1
+        return 1
+    end
+
+    echo -sn (string unescape (string join '' '\x1B[' $code))
 end
