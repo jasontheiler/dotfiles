0ac97cf5fc725b5d4b45400830f35d0892acf5a3
Jason Theiler <turret.jason@outlook.com>
2022-08-12 14:32:53 +0200

    feat: add Docker abbreviations

--- docker/.config/fish/conf.d/docker.fish
+++ docker/.config/fish/conf.d/docker.fish
@@ -1,3 +1,8 @@
 # Adds Docker abbreviations.
 abbr -a dps docker ps
 abbr -a dc docker compose
+abbr -a dcu docker compose up
+abbr -a dcul docker compose up -d \&\& docker compose logs -f
+abbr -a dcr docker compose restart
+abbr -a dcrl docker compose restart \&\& docker compose logs -f
+abbr -a dcd docker compose down
