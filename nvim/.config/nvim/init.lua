ae0ccc9c6e1030c2561950d20635977cf65ce48b
Jason Theiler <turret.jason@outlook.com>
2022-09-27 17:51:05 +0200

    refactor: restructure Nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,12 +6,10 @@ require("plugins/treesitter")
 
 local cmd = vim.cmd
 local opt = vim.opt
-local wo = vim.wo
 
 opt.history = 1000
 opt.number = true
 opt.relativenumber = true
--- opt.signcolumn = "yes"
+opt.signcolumn = "yes"
+opt.fillchars = "eob: "
 opt.mouse = "a"
-
-wo.fillchars = "eob: "
