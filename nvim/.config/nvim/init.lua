f8c95032cff46f8176bbf4db14a412fafefdc8b6
Jason Theiler <turret.jason@outlook.com>
2022-09-27 02:38:03 +0200

    feat: update Nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,7 +3,10 @@ require("plugins/lualine")
 
 local cmd = vim.cmd
 local opt = vim.opt
+local wo = vim.wo
 
 opt.history = 1000
 opt.number = true
 opt.relativenumber = true
+
+wo.fillchars = "eob: "
