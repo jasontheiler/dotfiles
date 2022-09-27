3222705af810887f13ececeedea6800b00958569
Jason Theiler <turret.jason@outlook.com>
2022-09-27 02:37:52 +0200

    feat: update Nvim lualine config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,27 @@
 require("lualine").setup({
-
+  options = {
+    theme = "horizon",
+    component_separators = '',
+    section_separators = ' ',
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = {},
+    lualine_c = {
+      'branch',
+      'diff',
+      {
+        'filename',
+        symbols = {
+          modified = "●",
+          readonly = "(readonly)",
+          unnamed = "unnamed file",
+        },
+      },
+      'diagnostics',
+    },
+    lualine_x = { 'encoding', 'filetype' },
+    lualine_y = {},
+    lualine_z = {},
+  }
 })
