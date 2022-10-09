fec85f901e3a0a4cce0cfb4d560310706da30318
Jason Theiler <turret.jason@outlook.com>
2022-10-09 22:39:07 +0200

    feat: update Neovim config

--- nvim/.config/nvim/lua/plugins/configs/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/configs/treesitter.lua
@@ -30,13 +30,4 @@ treesitter.setup({
   indent = {
     enable = true,
   },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
-    },
-  },
 })
