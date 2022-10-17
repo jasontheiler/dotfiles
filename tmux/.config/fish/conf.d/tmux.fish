6a70eb319097580285708af7700d1163bc61df33
Jason Theiler <turret.jason@outlook.com>
2022-10-18 01:34:27 +0200

    feat: update tmux config

--- tmux/.config/fish/conf.d/tmux.fish
+++ tmux/.config/fish/conf.d/tmux.fish
@@ -1,8 +1,4 @@
-# Attaches to the default tmux session or creates a new one.
+# Attaches to a default tmux session or creates a new one, if one doesn't already exist.
 if not set -q TMUX
-    if tmux has -t default 2>/dev/null
-        # exec tmux attach -t default
-    else
-        # exec tmux new -s default
-    end
+    exec tmux new-session -As default -c $HOME
 end
