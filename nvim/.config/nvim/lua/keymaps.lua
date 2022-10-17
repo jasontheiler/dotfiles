7dfb7a7d609ce5f2c9ff2a9c82b8f0e3a94bac43
Jason Theiler <turret.jason@outlook.com>
2022-10-17 03:14:22 +0200

    feat: update Neovim config

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -8,6 +8,10 @@ keymap("n", "<Leader>qf", ":q!<CR>")
 keymap("n", "<Leader>qa", ":qa<CR>")
 
 keymap("n", "<Leader>w", "<C-w>")
+keymap("n", { "<C-w><C-h>", "<Leader>w<C-h>" }, "<C-w><")
+keymap("n", { "<C-w><C-j>", "<Leader>w<C-j>" }, "<C-w>+")
+keymap("n", { "<C-w><C-k>", "<Leader>w<C-k>" }, "<C-w>-")
+keymap("n", { "<C-w><C-l>", "<Leader>w<C-l>" }, "<C-w>>")
 
 keymap("n", "[d", vim.diagnostic.goto_prev)
 keymap("n", "]d", vim.diagnostic.goto_next)
