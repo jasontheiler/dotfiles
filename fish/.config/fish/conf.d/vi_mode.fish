0b49721f8ac749c324fb83025b5f438c9b84e1c4
Jason Theiler <turret.jason@outlook.com>
2022-10-21 02:33:34 +0200

    feat: update fish config

--- fish/.config/fish/conf.d/vi_mode.fish
+++ fish/.config/fish/conf.d/vi_mode.fish
@@ -3,6 +3,7 @@
 fish_vi_key_bindings
 
 # Changes the cursor's shape depending on the current Vi mode.
+set fish_vi_force_cursor
 set fish_cursor_default block
 set fish_cursor_insert line
 set fish_cursor_replace_one underscore
