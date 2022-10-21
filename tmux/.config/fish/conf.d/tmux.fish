854511235cfa28d1af04900f15c216206d868f8f
Jason Theiler <turret.jason@outlook.com>
2022-10-21 02:47:25 +0200

    feat: update tmux config

--- tmux/.config/fish/conf.d/tmux.fish
+++ tmux/.config/fish/conf.d/tmux.fish
@@ -1,3 +1,4 @@
 # Adds tmux abbreviations.
+abbr -a txcs tmux_create_session
 abbr -a txac tmux choose-session -ZNF \"#{session_path}\" \&\& tmux attach-session
 abbr -a txcd cd \(tmux display-message -p \"#{session_path}\"\)
--- tmux/.config/fish/functions/tmux_create_session.fish
+++ tmux/.config/fish/functions/tmux_create_session.fish
@@ -1,15 +1,34 @@
-# Creates a new tmux session with the current working directory as its name and attaches to it.
-function tmux_create_session \
-    -d "Creates a new tmux session with the current working directory as its name and attaches to it."
-    set -l session_name (tmux new-session -dP -s (string replace $HOME "~" $PWD) -c $PWD 2>/dev/null)
-
-    if test $status -ne 0
-        alert error "Session does already exist"
+function tmux_create_session
+    if not test -d $argv[1]
+        alert error "Directory does not exist: " `$argv[1]`
+        return 1
     end
 
+    set -l working_dir (realpath $argv[1])
+
+    if test $working_dir = /
+        alert error "The root directory must not be used!"
+        return 1
+    end
+
+    set -l session_paths (tmux list-sessions -F "#{session_path}" 2>/dev/null)
+
+    if test (count $session_paths) -eq 0
+        tmux new-session -s $working_dir_segs[-1] -c $working_dir
+        return
+    else if contains $working_dir $session_paths
+        alert error "A session with this working directory already exists!"
+        return 1
+    end
+
+    set -l working_dir_segs (string split / (string trim -c / $working_dir))
+    set -l session_names (tmux list-sessions -F "#{session_name}")
+
+    tmux new-session -d -s $working_dir_segs[-1] -c $working_dir
+
     if set -q TMUX
-        tmux switch-client -t $session_name
+        tmux switch-client -t $working_dir_segs[-1]
     else
-        tmux attach-session -t $session_name
+        tmux attach-session -t $working_dir_segs[-1]
     end
 end
