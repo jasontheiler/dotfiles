9fd56c013bf42b4cbb90e25a22bdd6415cdb356a
Jason Theiler <turret.jason@outlook.com>
2022-10-22 02:05:02 +0200

    feat: update Neovim config

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,16 +1,16 @@
 local default_augroup = vim.api.nvim_create_augroup("default", {})
 
-vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
-  group = default_augroup,
-  callback = function(data)
-    if _G.cmdline_auto_clear_timer then
-      _G.cmdline_auto_clear_timer:stop()
-    end
-
-    if data.event == "CmdlineLeave" then
-      _G.cmdline_auto_clear_timer = vim.defer_fn(function()
-        vim.api.nvim_echo({ { "" } }, false, {})
-      end, 3000)
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+--   group = default_augroup,
+--   callback = function(data)
+--     if _G.cmdline_auto_clear_timer then
+--       _G.cmdline_auto_clear_timer:stop()
+--     end
+-- 
+--     if data.event == "CmdlineLeave" then
+--       _G.cmdline_auto_clear_timer = vim.defer_fn(function()
+--         vim.api.nvim_echo({ { "" } }, false, {})
+--       end, 3000)
+--     end
+--   end,
+-- })
