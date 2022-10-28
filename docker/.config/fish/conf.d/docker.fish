64a2032f22b11267c27f65a4c1e11eb6dbddbdb2
Jason Theiler <turret.jason@outlook.com>
2022-10-29 01:36:06 +0200

    feat: update Docker abbreviations

--- docker/.config/fish/conf.d/docker.fish
+++ docker/.config/fish/conf.d/docker.fish
@@ -1,10 +1,9 @@
 # Adds Docker abbreviations.
-abbr -a dps docker ps
+abbr -a dps 'docker ps --format "table {{.ID}}\t{{.Names}}\t{{.RunningFor}}\t{{.Status}}"'
+abbr -a dpsd 'docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Ports}}\t{{.Networks}}\t{{.Size}}"'
 abbr -a dc docker compose
-abbr -a dcu docker compose up
-abbr -a dcub docker compose up --build
-abbr -a dcud docker compose up -d
-abbr -a dcubd docker compose up -d --build
+abbr -a dcu docker compose up -d
+abbr -a dcub docker compose up -d --build
 abbr -a dcl docker compose logs -f
 abbr -a dcul docker compose up -d \&\& docker compose logs -f
 abbr -a dcubl docker compose up -d --build \&\& docker compose logs -f
