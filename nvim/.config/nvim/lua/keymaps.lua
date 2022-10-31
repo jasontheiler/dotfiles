20c40cc8fb232872a7df16affdd59e193d1d4dca
Jason Theiler <turret.jason@outlook.com>
2022-10-31 02:32:18 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -18,9 +18,4 @@ keymap("n", "[d", vim.diagnostic.goto_prev)
 keymap("n", "]d", vim.diagnostic.goto_next)
 keymap("n", "<Leader>d", vim.diagnostic.open_float)
 
-keymap("i", "<C-h>", "<Left>")
-keymap("i", "<C-j>", "<Down>")
-keymap("i", "<C-k>", "<Up>")
-keymap("i", "<C-l>", "<Right>")
-
 keymap("t", "<Esc>", "<C-\\><C-n>")
