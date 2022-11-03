694f4b9955bf0ce63e452d70df9099baa41dc022
Jason Theiler <turret.jason@outlook.com>
2022-11-04 00:57:41 +0100

    feat: update fish config

---
+++ fish/.config/fish/functions/time_out.fish
@@ -0,0 +1,41 @@
+function time_out
+    set -l bins \
+        bat \
+        brew \
+        btm \
+        curl \
+        docker \
+        exa \
+        fish \
+        fnm \
+        git \
+        gpg \
+        lazygit \
+        mkcert \
+        nvim \
+        oha \
+        paru \
+        ranger \
+        rg \
+        rustup \
+        scp \
+        ssh \
+        starship \
+        stow \
+        tmux
+
+    set -l installed_count 0
+
+    for bin in (string pad $bins)
+        echo -sn $bin " "
+
+        if fish -c "command -q $bin"
+            set installed_count (math $installed_count + 1)
+            echo -s (set_color green)  (set_color normal)
+        else
+            echo -s (set_color red)  (set_color normal)
+        end
+    end
+
+    echo -s \n $installed_count / (count $bins) " installed"
+end
