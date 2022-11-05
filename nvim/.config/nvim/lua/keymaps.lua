9ca771712f6b1c775048ce9d317b5c698bea675f
Jason Theiler <turret.jason@outlook.com>
2022-11-05 16:33:52 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -7,12 +7,13 @@ keymap("n", "<Leader>qq", ":q<CR>")
 keymap("n", "<Leader>qf", ":q!<CR>")
 keymap("n", "<Leader>qa", ":qa<CR>")
 
+keymap("n", "<Leader>bx", ":bd<CR>")
+
 keymap("n", "<Leader>w", "<C-w>")
 keymap("n", { "<C-w><C-h>", "<Leader>w<C-h>" }, "<C-w><")
 keymap("n", { "<C-w><C-j>", "<Leader>w<C-j>" }, "<C-w>+")
 keymap("n", { "<C-w><C-k>", "<Leader>w<C-k>" }, "<C-w>-")
 keymap("n", { "<C-w><C-l>", "<Leader>w<C-l>" }, "<C-w>>")
-keymap("n", { "<C-w><C-x>", "<C-w>x", "<Leader>wx" }, ":bd<CR>")
 
 keymap("n", "[d", vim.diagnostic.goto_prev)
 keymap("n", "]d", vim.diagnostic.goto_next)
--- nvim/.config/nvim/lua/plugins/configs/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/configs/bufferline.lua
@@ -4,12 +4,18 @@ local keymap = require("utils").keymap
 -- See: https://github.com/akinsho/bufferline.nvim#usage
 bufferline.setup({
   options = {
+    numbers = function(opts)
+      return string.format("%s:", opts.ordinal)
+    end,
     show_buffer_close_icons = false,
     show_close_icon = false,
     right_mouse_command = nil,
     middle_mouse_command = "bdelete! %d",
   },
 })
+
+keymap("n", "<Leader>bh", function() bufferline.move(-1) end)
+keymap("n", "<Leader>bl", function() bufferline.move(1) end)
 
 keymap("n", "<Tab>", function() bufferline.cycle(1) end)
 keymap("n", "<S-Tab>", function() bufferline.cycle(-1) end)
