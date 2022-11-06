b6e794fff58d563a95d07a9d95de51f736b5fdf4
Jason Theiler <turret.jason@outlook.com>
2022-11-06 02:40:39 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/plugins/configs/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/configs/bufferline.lua
@@ -17,5 +17,15 @@ bufferline.setup({
 keymap("n", "<Leader>bh", function() bufferline.move(-1) end)
 keymap("n", "<Leader>bl", function() bufferline.move(1) end)
 
+keymap("n", "<Leader>b1", function() bufferline.go_to_buffer(1, true) end)
+keymap("n", "<Leader>b2", function() bufferline.go_to_buffer(2, true) end)
+keymap("n", "<Leader>b3", function() bufferline.go_to_buffer(3, true) end)
+keymap("n", "<Leader>b4", function() bufferline.go_to_buffer(4, true) end)
+keymap("n", "<Leader>b5", function() bufferline.go_to_buffer(5, true) end)
+keymap("n", "<Leader>b6", function() bufferline.go_to_buffer(6, true) end)
+keymap("n", "<Leader>b7", function() bufferline.go_to_buffer(7, true) end)
+keymap("n", "<Leader>b8", function() bufferline.go_to_buffer(8, true) end)
+keymap("n", "<Leader>b9", function() bufferline.go_to_buffer(9, true) end)
+
 keymap("n", "<Tab>", function() bufferline.cycle(1) end)
 keymap("n", "<S-Tab>", function() bufferline.cycle(-1) end)
