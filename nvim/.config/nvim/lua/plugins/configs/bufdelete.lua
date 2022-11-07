fb6c12a8bcfc8547b535f7a5e6281d7f68c52962
Jason Theiler <turret.jason@outlook.com>
2022-11-07 16:03:06 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/plugins/configs/bufdelete.lua
+++ nvim/.config/nvim/lua/plugins/configs/bufdelete.lua
@@ -2,5 +2,5 @@ local bufdelete = require("bufdelete")
 local keymap = require("utils").keymap
 
 -- See: https://github.com/famiu/bufdelete.nvim#usage
-keymap("n", "<Leader>bx", function() bufdelete.bufwipeout(0) end)
-keymap("n", "<Leader>bX", function() bufdelete.bufwipeout(0, true) end)
+keymap("n", "<Leader>bx", function() bufdelete.bufdelete(0) end)
+keymap("n", "<Leader>bX", function() bufdelete.bufdelete(0, true) end)
