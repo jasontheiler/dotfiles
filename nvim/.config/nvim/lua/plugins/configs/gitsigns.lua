0b07f97e88fb4414e1db7c16e003358a7cdf8acf
Jason Theiler <turret.jason@outlook.com>
2022-11-07 01:34:03 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/plugins/configs/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/configs/gitsigns.lua
@@ -1,4 +1,16 @@
 local gitsigns = require("gitsigns")
+local keymap = require("utils").keymap
 
 -- See: https://github.com/lewis6991/gitsigns.nvim#usage
-gitsigns.setup({})
+gitsigns.setup({
+  on_attach = function(buffer)
+    local gs = package.loaded.gitsigns
+    local opts = { buffer = buffer }
+
+    keymap("n", "[g", gs.prev_hunk, opts)
+    keymap("n", "]g", gs.next_hunk, opts)
+    keymap("n", "<Leader>gb", gs.blame_line, opts)
+    keymap("n", "<Leader>gp", gs.preview_hunk, opts)
+    keymap("n", "<Leader>gu", gs.reset_hunk, opts)
+  end,
+})
