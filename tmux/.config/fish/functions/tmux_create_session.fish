721291cedcfa8312fc43c130d23a57967a8111e1
Jason Theiler <turret.jason@outlook.com>
2022-11-13 14:49:02 +0100

    feat: update tmux config

--- tmux/.config/fish/functions/tmux_create_session.fish
+++ tmux/.config/fish/functions/tmux_create_session.fish
@@ -1,10 +1,16 @@
 function tmux_create_session
-    if not test -d $argv[1]
-        alert error "Directory does not exist: " `$argv[1]`
+    set -l working_dir $argv[1]
+
+    if test -z $working_dir
+        set working_dir (pwd)
+    end
+
+    if not test -d $working_dir
+        alert error "Directory does not exist: " `$working_dir`
         return 1
     end
 
-    set -l working_dir (realpath $argv[1])
+    set working_dir (realpath $working_dir)
     set -l session_paths (tmux list-sessions -F "#{session_path}" 2>/dev/null)
 
     if contains $working_dir $session_paths
