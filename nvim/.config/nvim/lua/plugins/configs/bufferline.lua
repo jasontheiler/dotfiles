42feaeb8f152df8ff878be17f5df93e87f1b8de1
Jason Theiler <turret.jason@outlook.com>
2022-11-23 02:25:34 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/plugins/configs/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/configs/bufferline.lua
@@ -13,18 +13,37 @@ bufferline.setup({
   },
 })
 
-keymap("n", "<Leader>bh", function() bufferline.move(-1) end)
-keymap("n", "<Leader>bl", function() bufferline.move(1) end)
+keymap("n", "<Leader>1", function() bufferline.go_to(1, true) end)
+keymap("n", "<Leader>2", function() bufferline.go_to(2, true) end)
+keymap("n", "<Leader>3", function() bufferline.go_to(3, true) end)
+keymap("n", "<Leader>4", function() bufferline.go_to(4, true) end)
+keymap("n", "<Leader>5", function() bufferline.go_to(5, true) end)
+keymap("n", "<Leader>6", function() bufferline.go_to(6, true) end)
+keymap("n", "<Leader>7", function() bufferline.go_to(7, true) end)
+keymap("n", "<Leader>8", function() bufferline.go_to(8, true) end)
+keymap("n", "<Leader>9", function() bufferline.go_to(9, true) end)
 
-keymap("n", "<Leader>b1", function() bufferline.go_to(1, true) end)
-keymap("n", "<Leader>b2", function() bufferline.go_to(2, true) end)
-keymap("n", "<Leader>b3", function() bufferline.go_to(3, true) end)
-keymap("n", "<Leader>b4", function() bufferline.go_to(4, true) end)
-keymap("n", "<Leader>b5", function() bufferline.go_to(5, true) end)
-keymap("n", "<Leader>b6", function() bufferline.go_to(6, true) end)
-keymap("n", "<Leader>b7", function() bufferline.go_to(7, true) end)
-keymap("n", "<Leader>b8", function() bufferline.go_to(8, true) end)
-keymap("n", "<Leader>b9", function() bufferline.go_to(9, true) end)
+local move_to = function(new_idx)
+  local current_elements = bufferline.get_elements().elements
+  local current_idx = nil
 
--- keymap("n", "<Tab>", function() bufferline.cycle(1) end)
--- keymap("n", "<S-Tab>", function() bufferline.cycle(-1) end)
+  for idx, element in pairs(current_elements) do
+    if element.path == vim.api.nvim_buf_get_name(0) then
+      current_idx = idx
+    end
+  end
+
+  if current_idx ~= nil then
+    bufferline.move(math.min(new_idx - current_idx, #current_elements - current_idx))
+  end
+end
+
+keymap("n", "<Leader>b1", function() move_to(1) end)
+keymap("n", "<Leader>b2", function() move_to(2) end)
+keymap("n", "<Leader>b3", function() move_to(3) end)
+keymap("n", "<Leader>b4", function() move_to(4) end)
+keymap("n", "<Leader>b5", function() move_to(5) end)
+keymap("n", "<Leader>b6", function() move_to(6) end)
+keymap("n", "<Leader>b7", function() move_to(7) end)
+keymap("n", "<Leader>b8", function() move_to(8) end)
+keymap("n", "<Leader>b9", function() move_to(9) end)
