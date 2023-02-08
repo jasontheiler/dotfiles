b52b8420919d7020faaca16a4b5f270f69d592d7
Jason Theiler <turret.jason@outlook.com>
2023-02-08 20:04:31 +0100

    feat: update fish config

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -1,15 +1,19 @@
 # Enables colorization of some common commands.
-alias ls="ls --color=auto"
-alias dir="dir --color=auto"
-alias vdir="vdir --color=auto"
+alias ls 'ls --color=auto'
+alias dir 'dir --color=auto'
+alias vdir 'vdir --color=auto'
 
-alias grep="grep --color=auto"
-alias fgrep="fgrep --color=auto"
-alias egrep="egrep --color=auto"
+alias grep 'grep --color=auto'
+alias fgrep 'fgrep --color=auto'
+alias egrep 'egrep --color=auto'
 
-alias ip="ip --color=auto"
+alias ip 'ip --color=auto'
 
-alias l="ls -F"
-alias la="ls -FA"
-alias ll="ls -Fl"
-alias lla="ls -FlA"
+alias l 'ls -F'
+alias la 'ls -FA'
+alias ll 'ls -Fl'
+alias lla 'ls -FlA'
+
+if type -q gsed
+    alias sed 'gsed'
+end
