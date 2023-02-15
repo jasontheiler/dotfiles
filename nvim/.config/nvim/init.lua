88dedd51de8047e477d8def6e60804727df88ca4
Jason Theiler <turret.jason@outlook.com>
2023-02-16 00:42:42 +0100

    feat: update Neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("opts")
 require("keymaps")
+require("user-commands")
 require("autocmds")
 require("plugins")
---
+++ nvim/.config/nvim/lua/user-commands.lua
@@ -0,0 +1,57 @@
+vim.api.nvim_create_user_command("Update", function()
+  local is_packer_installed, packer = pcall(require, "packer")
+
+  if is_packer_installed then
+    packer.sync()
+  end
+
+  local is_treesitter_installed = pcall(require, "nvim-treesitter")
+
+  if is_treesitter_installed then
+    vim.api.nvim_command("TSUpdate")
+  end
+
+  local is_mason_installed, mason_registry = pcall(require, "mason-registry")
+
+  -- See: https://github.com/RubixDev/mason-update-all/blob/main/lua/mason-update-all/init.lua
+  if is_mason_installed then
+    local packages = mason_registry.get_installed_packages()
+    local has_new_versions = false
+    local running_updates = 0
+
+    for _, package in pairs(packages) do
+      package:check_new_version(function(is_new_version_available, version)
+        if is_new_version_available then
+          has_new_versions = true
+          running_updates = running_updates + 1
+
+          print(string.format(
+            "Updating Mason package `%s` from v%s to v%s",
+            package.name,
+            version.current_version,
+            version.latest_version
+          ))
+
+          package:install():on("close", function()
+            print(string.format(
+              "Updated Mason package `%s` from v%s to v%s",
+              package.name,
+              version.current_version,
+              version.latest_version
+            ))
+
+            running_updates = running_updates - 1
+
+            if running_updates == 0 then
+              print("All Mason packages are up-to-date!")
+            end
+          end)
+        end
+      end)
+    end
+
+    if not has_new_versions then
+      print("All Mason packages are up-to-date!")
+    end
+  end
+end, {})
