8f51b0f61ba4ccc5067636723b699e56e2026bad
Jason Theiler <turret.jason@outlook.com>
2023-03-07 01:22:09 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/plugins/configs/bufdelete.lua
+++ nvim/.config/nvim/lua/plugins/configs/bufdelete.lua
@@ -2,4 +2,10 @@ local bufdelete = require("bufdelete")
 local utils = require("utils")
 
 -- See: https://github.com/famiu/bufdelete.nvim#usage
-utils.keymap("n", "<Leader>bx", function() bufdelete.bufdelete(0) end)
+utils.keymap("n", "<Leader>bx", function()
+  if vim.bo.filetype == "" then
+    bufdelete.bufdelete(0, true)
+  else
+    bufdelete.bufdelete(0)
+  end
+end)
