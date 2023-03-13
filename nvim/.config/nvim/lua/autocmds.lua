048338b409d091d101b6b0900a9189b062bf5b9d
Jason Theiler <turret.jason@outlook.com>
2023-03-13 23:17:00 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -41,18 +41,3 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
     vim.highlight.on_yank({ higroup = "YankHighlight", timeout = 250 })
   end,
 })
-
--- vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
---   group = default_augroup,
---   callback = function(data)
---     if _G.cmdline_auto_clear_timer then
---       _G.cmdline_auto_clear_timer:stop()
---     end
---
---     if data.event == "CmdlineLeave" then
---       _G.cmdline_auto_clear_timer = vim.defer_fn(function()
---         vim.api.nvim_echo({ { "" } }, false, {})
---       end, 3000)
---     end
---   end,
--- })
