e62572086385885299076b7d841a152d82fa8f43
Jason Theiler <turret.jason@outlook.com>
2023-03-13 23:02:43 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,5 +1,40 @@
 local default_augroup = vim.api.nvim_create_augroup("default", {})
 
+vim.api.nvim_create_autocmd({ "BufEnter", "ModeChanged" }, {
+  group = default_augroup,
+  callback = function()
+    local mode = vim.api.nvim_get_mode().mode
+    local mode_hl_suffixes = {
+      ["v"] = "Visual",
+      ["V"] = "Visual",
+      ["Vs"] = "Visual",
+      ["\22"] = "Visual",
+      ["\22s"] = "Visual",
+      ["s"] = "Visual",
+      ["S"] = "Visual",
+      ["\19"] = "Visual",
+      ["i"] = "Insert",
+      ["ic"] = "Insert",
+      ["ix"] = "Insert",
+      ["r"] = "Replace",
+      ["R"] = "Replace",
+      ["Rc"] = "Replace",
+      ["Rx"] = "Replace",
+      ["Rv"] = "Replace",
+      ["Rvc"] = "Replace",
+      ["Rvx"] = "Replace",
+    }
+    local hl_exists, hl = pcall(
+      vim.api.nvim_get_hl_by_name,
+      "CursorLineNr" .. (mode_hl_suffixes[mode] or "Normal"),
+      true
+    )
+    if hl_exists then
+      vim.api.nvim_set_hl(0, "CursorLineNr", hl)
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   group = default_augroup,
   callback = function()
