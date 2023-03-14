db0ef3f9dbce06917811c9d758041526a5c748ea
Jason Theiler <turret.jason@outlook.com>
2023-03-15 00:34:57 +0100

    feat: update Docker config

--- docker/.config/fish/conf.d/docker.fish
+++ docker/.config/fish/conf.d/docker.fish
@@ -6,6 +6,4 @@ abbr -a dcub docker compose up -d --build
 abbr -a dcl docker compose logs -f
 abbr -a dcul docker compose up -d \&\& docker compose logs -f
 abbr -a dcubl docker compose up -d --build \&\& docker compose logs -f
-abbr -a dcr docker compose restart
-abbr -a dcrl docker compose restart \&\& docker compose logs -f
 abbr -a dcd docker compose down
