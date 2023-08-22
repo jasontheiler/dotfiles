abde8233ce00e9f2dab55560de242f4abad13db8
Jason Theiler <turret.jason@outlook.com>
2023-08-22 11:34:28 +0200

    feat: update Neovim config

--- nvim/.config/nvim/lua/opts.lua
+++ nvim/.config/nvim/lua/opts.lua
@@ -64,3 +64,9 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
   vim.lsp.handlers.signature_help,
   { border = "single" }
 )
+
+vim.filetype.add({
+  pattern = {
+    [".*[tT]iltfile"] = "starlark",
+  },
+})
