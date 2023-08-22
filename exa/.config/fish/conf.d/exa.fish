6c5bc8f913ee966881d48a6e69097e7e791ce585
Jason Theiler <turret.jason@outlook.com>
2023-08-22 11:55:02 +0200

    feat: update exa config

--- exa/.config/fish/conf.d/exa.fish
+++ exa/.config/fish/conf.d/exa.fish
@@ -5,7 +5,10 @@ abbr -a l exa -Fs type --tree -L 1
 abbr -a la exa -Fas type --tree -L 1
 abbr -a ll exa -Fls type --git --tree -L 1
 abbr -a lla exa -Flas type --git --tree -L 1
-abbr -a t exa -Fs type --tree -L
-abbr -a ta exa -Fas type --tree -L
-abbr -a tl exa -Fls type --git --tree -L
-abbr -a tla exa -Flas type --git --tree -L
+
+for i in 2 3 4 5 6 7 8 9
+    abbr -a l$i exa -Fs type --tree -L $i
+    abbr -a la$i exa -Fas type --tree -L $i
+    abbr -a ll$i exa -Fls type --git --tree -L $i
+    abbr -a lla$i exa -Flas type --git --tree -L $i
+end
