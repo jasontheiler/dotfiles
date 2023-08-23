610b793fe8b5fafd34deda80c9c21cfab272734a
Jason Theiler <turret.jason@outlook.com>
2023-08-24 01:12:30 +0200

    feat: update Kubernetes config

--- kubernetes/.config/fish/conf.d/kubernetes.fish
+++ kubernetes/.config/fish/conf.d/kubernetes.fish
@@ -1,6 +1,7 @@
 abbr -a k kubectl
 abbr -a kga kubectl get all
 abbr -a kgp kubectl get pods
+abbr -a kgd kubectl get deployments
 abbr -a kgs kubectl get services
 abbr -a kgns kubectl get namespaces
 abbr -a kns kubectl config set-context --current --namespace
