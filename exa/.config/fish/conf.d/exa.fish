06c08a6e2ebae697b5f8e8c449605a4ca57d56cf
Jason Theiler <turret.jason@outlook.com>
2023-08-26 22:03:57 +0200

    feat: update exa config

--- exa/.config/fish/conf.d/exa.fish
+++ exa/.config/fish/conf.d/exa.fish
@@ -2,8 +2,8 @@
 set -x EXA_COLORS "xx=1;30"
 
 for i in 1 2 3 4 5 6 7 8 9
-    abbr -a l(string replace 1 "" $i) exa -Fs type --tree -L $i
-    abbr -a la(string replace 1 "" $i) exa -Fas type --tree -L $i
-    abbr -a ll(string replace 1 "" $i) exa -Fls type --git --tree -L $i
-    abbr -a lla(string replace 1 "" $i) exa -Flas type --git --tree -L $i
+    abbr -a l(string replace -r "^1\$" "" $i) exa -Fs type --tree -L $i
+    abbr -a la(string replace -r "^1\$" "" $i) exa -Fas type --tree -L $i
+    abbr -a ll(string replace -r "^1\$" "" $i) exa -Fls type --git --tree -L $i
+    abbr -a lla(string replace -r "^1\$" "" $i) exa -Flas type --git --tree -L $i
 end
