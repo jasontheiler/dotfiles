d22256ea948c484320a8cf706eaa9145ac6ccf28
Jason Theiler <turret.jason@outlook.com>
2023-08-28 02:29:29 +0200

    feat: update fish config

--- fish/.config/fish/conf.d/$paths.fish
+++ fish/.config/fish/conf.d/$paths.fish
@@ -1,4 +1,8 @@
 # Appends directories to the user paths.
 set -a fish_user_paths \
-    ~/.local/bin \
-    ~/.cargo/bin
+    ~/.local/bin/ \
+    ~/.cargo/bin/
+
+if set -q WSL_DISTRO_NAME
+    set -a fish_user_paths "/mnt/c/Program Files/win32yank/"
+end
