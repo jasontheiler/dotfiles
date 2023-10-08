b0d824adb186ee4c93db0e0973e9119d0a747bb7
Jason Theiler <turret.jason@outlook.com>
2023-10-09 01:58:04 +0200

    feat: update tmux config

--- tmux/.config/fish/conf.d/tmux.fish
+++ tmux/.config/fish/conf.d/tmux.fish
@@ -1,4 +1,4 @@
 abbr -a txa tmux attach-session
-abbr -a txcs tmux_create_session
+abbr -a txcs tmuxion create
 abbr -a txcd cd \(tmux display-message -p \"#{session_path}\"\)
 abbr -a txks tmux kill-session
