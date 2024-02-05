d3f0d505565b27b737f0b1a4ad73ba027d3fa4d1
Jason Theiler <turret.jason@outlook.com>
2024-02-05 13:18:29 +0100

    feat: update eza config

--- eza/.config/fish/conf.d/eza.fish
+++ eza/.config/fish/conf.d/eza.fish
@@ -1,6 +1,6 @@
 for i in (seq 1 9)
-    abbr -a l(string replace -r '^1$' "" $i) eza -Fs type --tree -L $i
-    abbr -a la(string replace -r '^1$' "" $i) eza -Fas type --tree -L $i
-    abbr -a ll(string replace -r '^1$' "" $i) eza -Fls type --git --tree -L $i
-    abbr -a lla(string replace -r '^1$' "" $i) eza -Flas type --git --tree -L $i
+    abbr -a l(string replace -r '^1$' "" $i) eza -F auto -s type -TL $i
+    abbr -a la(string replace -r '^1$' "" $i) eza -aF auto -s type -TL $i
+    abbr -a ll(string replace -r '^1$' "" $i) eza -lF auto -s type --git -TL $i
+    abbr -a lla(string replace -r '^1$' "" $i) eza -laF auto -s type --git -TL $i
 end
