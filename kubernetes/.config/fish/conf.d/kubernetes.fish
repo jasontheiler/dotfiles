35f815fb74932c77c721fe3fc1ff297408ab985d
Jason Theiler <turret.jason@outlook.com>
2024-02-27 13:07:22 +0100

    feat: update Kubernetes config

--- kubernetes/.config/fish/conf.d/kubernetes.fish
+++ kubernetes/.config/fish/conf.d/kubernetes.fish
@@ -1,7 +1,6 @@
 abbr -a k kubectl
 abbr -a kaf kubectl apply -f
 abbr -a kak kubectl apply -k
-abbr -a kg kubectl get
 abbr -a kga kubectl get all
 abbr -a kgp kubectl get pods
 abbr -a kgrs kubectl get replicasets
