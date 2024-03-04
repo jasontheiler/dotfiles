d594bafb2011f829ca074c2f27abcb1be194e2db
Jason Theiler <turret.jason@outlook.com>
2024-03-04 10:52:26 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/opts.lua
+++ nvim/.config/nvim/lua/opts.lua
@@ -19,6 +19,7 @@ vim.opt.signcolumn = "yes"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.wrap = false
+vim.opt.tabstop = 4
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 16
 vim.opt.cursorline = true
