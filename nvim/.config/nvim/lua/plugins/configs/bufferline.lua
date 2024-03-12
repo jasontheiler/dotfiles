6de5c06511fffb4d168a004e72696931b5721aee
Jason Theiler <turret.jason@outlook.com>
2024-03-12 14:25:41 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/plugins/configs/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/configs/bufferline.lua
@@ -14,15 +14,9 @@ bufferline.setup({
   },
 })
 
-utils.keymap("n", "<Leader>1", function() bufferline.go_to(1, true) end)
-utils.keymap("n", "<Leader>2", function() bufferline.go_to(2, true) end)
-utils.keymap("n", "<Leader>3", function() bufferline.go_to(3, true) end)
-utils.keymap("n", "<Leader>4", function() bufferline.go_to(4, true) end)
-utils.keymap("n", "<Leader>5", function() bufferline.go_to(5, true) end)
-utils.keymap("n", "<Leader>6", function() bufferline.go_to(6, true) end)
-utils.keymap("n", "<Leader>7", function() bufferline.go_to(7, true) end)
-utils.keymap("n", "<Leader>8", function() bufferline.go_to(8, true) end)
-utils.keymap("n", "<Leader>9", function() bufferline.go_to(9, true) end)
+for i = 1, 9 do
+  utils.keymap("n", "<leader>" .. i, function() bufferline.go_to(i, true) end, "which_key_ignore")
+end
 
 local move_to = function(new_index)
   local current_elements = bufferline.get_elements().elements
@@ -39,12 +33,6 @@ local move_to = function(new_index)
   end
 end
 
-utils.keymap("n", "<Leader>b1", function() move_to(1) end)
-utils.keymap("n", "<Leader>b2", function() move_to(2) end)
-utils.keymap("n", "<Leader>b3", function() move_to(3) end)
-utils.keymap("n", "<Leader>b4", function() move_to(4) end)
-utils.keymap("n", "<Leader>b5", function() move_to(5) end)
-utils.keymap("n", "<Leader>b6", function() move_to(6) end)
-utils.keymap("n", "<Leader>b7", function() move_to(7) end)
-utils.keymap("n", "<Leader>b8", function() move_to(8) end)
-utils.keymap("n", "<Leader>b9", function() move_to(9) end)
+for i = 1, 9 do
+  utils.keymap("n", "<Leader>b" .. i, function() move_to(i) end, "which_key_ignore")
+end
