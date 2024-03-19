3b9ee0f4a41b17a0a4d9283a3f171bec47f2e08e
Jason Theiler <turret.jason@outlook.com>
2024-03-19 01:29:53 +0100

    feat: update Neovim config

--- nvim/.config/nvim/lua/plugins/configs/noice.lua
+++ nvim/.config/nvim/lua/plugins/configs/noice.lua
@@ -3,24 +3,24 @@ local utils = require("utils")
 
 -- See: https://github.com/folke/noice.nvim
 noice.setup({
-  lsp = {
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
-    },
-  },
   presets = {
     lsp_doc_border = true,
   },
   cmdline = {
     format = {
-      cmdline = { title = " Command ", icon = "❯", icon_hl_group = "NoiceCmdlineIconCmdline" },
+      cmdline = { title = " Command ", icon = "❯" },
       filter = { icon = "!", icon_hl_group = "NoiceCmdlineIconFilter" },
       help = false,
       lua = false,
-      search_down = { icon = "/", icon_hl_group = "NoiceCmdlineIconSearch" },
-      search_up = { icon = "?", icon_hl_group = "NoiceCmdlineIconSearch" },
+      search_down = { icon = "/" },
+      search_up = { icon = "?" },
+    },
+  },
+  lsp = {
+    override = {
+      ["cmp.entry.get_documentation"] = true,
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
     },
   },
 })
