43f08afe3501c2d8e74fbd62ea80b00cc073b648
Jason Theiler <turret.jason@outlook.com>
2024-04-11 21:28:31 +0200

    feat: update Neovim config

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+--- Adds a new mapping with some customized defaults.
+---
+--- @param modes string|string[]
+--- @param lhs string
+--- @param rhs string|function
+--- @param desc string
+--- @param opts? vim.keymap.set.Opts
 M.keymap = function(modes, lhs, rhs, desc, opts)
   local default_opts = { noremap = true, silent = true, desc = desc }
   opts = vim.tbl_extend("force", default_opts, opts or {})
