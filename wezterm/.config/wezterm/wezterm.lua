12ab4e0dc20374a1a6d94f182ed39f4f84d0e32e
Jason Theiler <turret.jason@outlook.com>
2024-06-04 15:34:07 +0200

    feat: update WezTerm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,21 +7,27 @@ local config = wezterm.config_builder()
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 1
 config.window_close_confirmation = "NeverPrompt"
+config.window_padding = {
+  top = 8,
+  right = 8,
+  bottom = 0,
+  left = 8,
+}
+config.max_fps = 200
 config.enable_tab_bar = false
-config.max_fps = 120
 config.default_cursor_style = "SteadyBlock"
 config.cursor_blink_rate = 0
 config.font = wezterm.font_with_fallback({
-  { family = "CommitMono Nerd Font", scale = 1.07 },
+  "CommitMono Nerd Font",
   "MonoLisa Nerd Font",
 })
-config.font_size = 15
+config.font_size = 19
 config.bold_brightens_ansi_colors = "No"
 config.allow_square_glyphs_to_overflow_width = "Always"
 
 config.disable_default_key_bindings = true
 
-config.color_scheme = "Catppuccin Mocha Warm"
+config.color_scheme = "Catppuccin Mocha"
 config.color_schemes = {
   ["Catppuccin Mocha Warm"] = {
     foreground = "#E4CEA8",
@@ -53,12 +59,10 @@ config.color_schemes = {
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  config.webgpu_power_preference = "HighPerformance"
   config.default_prog = { "wsl", "--cd", "~" }
 end
 
 if wezterm.target_triple == "x86_64-apple-darwin" then
-  config.font_size = 20
 end
 
 return config
