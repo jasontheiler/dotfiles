93ecb2e3f368f2f169986d9a23ab4933ac2fb653
Jason Theiler <turret.jason@outlook.com>
2024-07-15 22:45:27 +0200

    feat: update Neovim config

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -5,23 +5,29 @@ return {
   config = function()
     local which_key = require("which-key")
 
-    -- See: https://github.com/folke/which-key.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
+    -- See: https://github.com/folke/which-key.nvim#%EF%B8%8F-configuration
     which_key.setup({
-      window = {
-        padding = { 1, 0, 2, 0 },
+      preset = "helix",
+      win = {
+        padding = { 1, 3 },
         border = "single",
       },
-      icons = { group = "" },
+      icons = {
+        group = "",
+        rules = false,
+        keys = {},
+      },
+      show_help = false,
     })
 
-    -- See: https://github.com/folke/which-key.nvim?tab=readme-ov-file#%EF%B8%8F-mappings
-    which_key.register({
-      ["<leader>b"] = { name = "Buffer" },
-      ["<leader>g"] = { name = "Git" },
-      ["<leader>l"] = { name = "LSP" },
-      ["<leader>r"] = { name = "Random" },
-      ["<leader>s"] = { name = "Search" },
-      ["<leader>w"] = { name = "Window" },
+    -- See: https://github.com/folke/which-key.nvim#%EF%B8%8F-setup
+    which_key.add({
+      { "<leader>b", group = "Buffer" },
+      { "<leader>g", group = "Git" },
+      { "<leader>l", group = "LSP" },
+      { "<leader>r", group = "Random" },
+      { "<leader>s", group = "Search" },
+      { "<leader>w", group = "Window" },
     })
   end,
 }
