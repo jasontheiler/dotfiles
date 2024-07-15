cfb1ee31cd9281a0ae946d442e2c63725da78506
Jason Theiler <turret.jason@outlook.com>
2024-07-15 21:18:17 +0200

    feat: update Neovim config

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -25,34 +25,62 @@ return {
         },
       },
       color_overrides = {
-        -- mocha = {
-        --   rosewater = "#DAA992",
-        --   flamingo = "#DAA992",
-        --   pink = "#C386BB", -- terminal magenta, terminal bright magenta
-        --   mauve = "#C386BB",
-        --   red = "#EA6962", -- terminal red, terminal bright red
-        --   maroon = "#D3869B",
-        --   peach = "#DD7F4E",
-        --   yellow = "#E8C677", -- terminal yellow, terminal bright yellow
-        --   green = "#A9B665", -- terminal green, terminal bright green
-        --   teal = "#89B482", -- terminal cyan, terminal bright cyan
-        --   sky = "#89B482",
-        --   sapphire = "#89B482",
-        --   blue = "#7DAEB3", -- terminal blue, terminal bright blue
-        --   lavender = "#BDAEC3",
-        --   text = "#E4CEA8", -- terminal white, terminal bright white, terminal foreground, terminal cursor
-        --   subtext1 = "#DCC6A0",
-        --   subtext0 = "#D4BE98",
-        --   overlay2 = "#877360", -- punctuation, brackets
-        --   overlay1 = "#7D6C60",
-        --   overlay0 = "#75615B", -- comments
-        --   surface2 = "#615556",
-        --   surface1 = "#534747", -- line numbers, selection, terminal black, terminal bright black
-        --   surface0 = "#433737",
-        --   base = "#211D1D", -- terminal background
-        --   mantle = "#191414",
-        --   crust = "#110D0D",
+        -- mocha = {                -- Warm
+        --   rosewater = "#DAA992", --
+        --   flamingo = "#DAA992",  --
+        --   pink = "#C386BB",      -- terminal magenta, terminal bright magenta
+        --   mauve = "#C386BB",     --
+        --   red = "#EA6962",       -- terminal red, terminal bright red
+        --   maroon = "#D3869B",    --
+        --   peach = "#DD7F4E",     --
+        --   yellow = "#E8C677",    -- terminal yellow, terminal bright yellow
+        --   green = "#A9B665",     -- terminal green, terminal bright green
+        --   teal = "#89B482",      -- terminal cyan, terminal bright cyan
+        --   sky = "#89B482",       --
+        --   sapphire = "#89B482",  --
+        --   blue = "#7DAEB3",      -- terminal blue, terminal bright blue
+        --   lavender = "#BDAEC3",  --
+        --   text = "#E4CEA8",      -- terminal white, terminal bright white, terminal foreground, terminal cursor
+        --   subtext1 = "#DCC6A0",  --
+        --   subtext0 = "#D4BE98",  --
+        --   overlay2 = "#877360",  -- punctuation, brackets
+        --   overlay1 = "#7D6C60",  --
+        --   overlay0 = "#75615B",  -- comments
+        --   surface2 = "#615556",  --
+        --   surface1 = "#534747",  -- line numbers, selection, terminal black, terminal bright black
+        --   surface0 = "#433737",  --
+        --   base = "#211D1D",      -- terminal background
+        --   mantle = "#191414",    --
+        --   crust = "#110D0D",     --
         -- },
+        mocha = {                -- Neo
+          rosewater = "#FFCDCD", --
+          flamingo = "#FFCDCD",  --
+          pink = "#E3AAFF",      -- terminal magenta, terminal bright magenta
+          mauve = "#E3AAFF",     --
+          red = "#FB7FB4",       -- terminal red, terminal bright red
+          maroon = "#F394B8",    --
+          peach = "#FFB79B",     --
+          yellow = "#F9E6B8",    -- terminal yellow, terminal bright yellow
+          green = "#B3F6C0",     -- terminal green, terminal bright green
+          teal = "#8CF8F7",      -- terminal cyan, terminal bright cyan
+          sky = "#8CF8F7",       --
+          sapphire = "#8CF8F7",  --
+          blue = "#A6DBFF",      -- terminal blue, terminal bright blue
+          lavender = "#C4BEFF",  --
+          text = "#E0E2EA",      -- terminal white, terminal bright white, terminal foreground, terminal cursor
+          subtext1 = "#B9BCC7",  --
+          subtext0 = "#A5A8B5",  --
+          overlay2 = "#9295A4",  -- punctuation, brackets
+          overlay1 = "#7E8192",  --
+          overlay0 = "#6B6D81",  -- comments
+          surface2 = "#585A6F",  --
+          surface1 = "#45475D",  -- line numbers, selection, terminal black, terminal bright black
+          surface0 = "#32334B",  --
+          base = "#14161B",      -- terminal background
+          mantle = "#0C0E13",    --
+          crust = "#04060B",     --
+        },
       },
       highlight_overrides = {
         mocha = function(palette)
@@ -79,7 +107,7 @@ return {
             FidgetGroup = { fg = palette.teal },
             FidgetIcon = { fg = palette.teal },
             FloatBorder = { fg = palette.lavender, bg = palette.none },
-            HeirlineBuflineBuf = { fg = palette.text },
+            HeirlineBuflineBuf = { fg = palette.text, style = { "bold" } },
             HeirlineBuflineBufInactive = { fg = palette.surface2 },
             HeirlineWinbar = { fg = palette.text },
             HeirlineWinbarInactive = { fg = palette.surface2 },
