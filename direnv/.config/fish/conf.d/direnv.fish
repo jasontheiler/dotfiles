1da8b2ff70fa420edb3a960f223adc2b21d9e8a5
Jason Theiler <turret.jason@outlook.com>
2024-10-17 18:08:04 +0200

    feat: update direnv config

--- direnv/.config/fish/conf.d/direnv.fish
+++ direnv/.config/fish/conf.d/direnv.fish
@@ -1,2 +1,10 @@
 # See: https://direnv.net/
 direnv hook fish | source
+
+# See: https://github.com/direnv/direnv/wiki/Quiet-or-Silence-direnv
+set -x DIRENV_LOG_FORMAT
+
+# See: https://github.com/direnv/direnv/wiki/Tmux
+if command -q tmux
+    alias tmux "direnv exec / tmux"
+end
