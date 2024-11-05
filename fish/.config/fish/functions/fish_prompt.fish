ad3d30b0b84d7f5cc5aed974672413186ff042f6
Jason Theiler <turret.jason@outlook.com>
2024-11-05 18:19:23 +0100

    feat: update fish config

---
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,54 @@
+function fish_prompt
+    set -l last_status $status
+    echo
+    fish_prompt_pwd
+    fish_prompt_git_branch
+    echo
+    fish_prompt_status $last_status
+    fish_prompt_char
+end
+
+function fish_prompt_pwd
+    set_color --bold cyan
+    echo -n (prompt_pwd)
+    if test $PWD != /
+        echo -n /
+    end
+    set_color normal
+end
+
+function fish_prompt_git_branch
+    set -l branch (git branch --show-current 2>/dev/null)
+    if test $status -gt 0
+        return
+    end
+    echo -n " on "
+    set_color --bold magenta
+    echo -n " $branch "
+    set_color normal
+end
+
+function fish_prompt_status
+    if test $argv[1] -eq 0
+        return
+    end
+    set_color --bold red
+    echo -n "[$argv[1]] "
+    set_color normal
+end
+
+function fish_prompt_char
+    switch $fish_bind_mode
+        case default
+            set_color blue
+        case insert
+            set_color green
+        case replace_one
+            set_color red
+        case visual
+            set_color magenta
+    end
+    set_color --bold
+    echo -n "λ "
+    set_color normal
+end
