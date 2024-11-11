b66e51690daf079740aebcd91e568cad94388aa6
Jason Theiler <turret.jason@outlook.com>
2024-11-11 17:11:20 +0100

    feat: update Kubernetes config

--- kubernetes/.config/fish/conf.d/kubernetes.fish
+++ kubernetes/.config/fish/conf.d/kubernetes.fish
@@ -1,24 +1,14 @@
 abbr -a k kubectl
 abbr -a kc kubectl create
-abbr -a kg kubectl get
+abbr -a kcf kubectl create -f
+abbr -a kck kubectl create -k
 abbr -a ka kubectl apply
 abbr -a kaf kubectl apply -f
 abbr -a kak kubectl apply -k
 abbr -a kd kubectl delete
 abbr -a kdf kubectl delete -f
 abbr -a kdk kubectl delete -k
-abbr -a kga kubectl get all
-abbr -a kgaa kubectl get -A all
-abbr -a kgp kubectl get pods
-abbr -a kgap kubectl get -A pods
-abbr -a kgrs kubectl get replicasets
-abbr -a kgars kubectl get -A replicasets
-abbr -a kgd kubectl get deployments
-abbr -a kgad kubectl get -A deployments
-abbr -a kgs kubectl get services
-abbr -a kgas kubectl get -A services
-abbr -a kgi kubectl get ingress
-abbr -a kgai kubectl get -A ingress
-abbr -a kgns kubectl get namespaces
-abbr -a kgn kubectl get nodes
-abbr -a kns kubectl config set-context --current --namespace
+abbr -a kg kubectl get
+abbr -a kctx kubectl config set-context
+abbr -a kcns kubectl config set-context --current --namespace
+abbr -a kctxu kubectl config unset current-context
