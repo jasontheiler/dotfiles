1cbfc79385f6241fc99ee12161f0cc227a92bfba
Jason Theiler <turret.jason@outlook.com>
2024-11-15 00:10:48 +0100

    feat: update fish config

---
+++ fish/.config/fish/conf.d/abbrs.fish
@@ -0,0 +1,4 @@
+abbr -a l ls -1vF --group-directories-first
+abbr -a la ls -1vFA --group-directories-first
+abbr -a ll ls -1vlF --group-directories-first
+abbr -a lla ls -1vlFA --group-directories-first
