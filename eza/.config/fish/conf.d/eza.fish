2a505abbe64bce71d6cb4102157b322463a86ae2
Jason Theiler <turret.jason@outlook.com>
2024-11-22 00:49:54 +0100

    feat: remove eza config

--- eza/.config/fish/conf.d/eza.fish
+++
@@ -1,6 +0,0 @@
-for i in (seq 1 9)
-    abbr -a l(string replace -r '^1$' "" $i) eza -F auto -s type -TL $i
-    abbr -a la(string replace -r '^1$' "" $i) eza -aF auto -s type -TL $i
-    abbr -a ll(string replace -r '^1$' "" $i) eza -lF auto -s type --git -TL $i
-    abbr -a lla(string replace -r '^1$' "" $i) eza -laF auto -s type --git -TL $i
-end
