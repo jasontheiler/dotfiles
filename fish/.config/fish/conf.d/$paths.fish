39096d95bd95586c1e1c4183dcd7888c91c314e8
Jason Theiler <turret.jason@outlook.com>
2024-11-24 17:16:35 +0100

    feat: update fish config

--- fish/.config/fish/conf.d/$paths.fish
+++ fish/.config/fish/conf.d/$paths.fish
@@ -1,10 +1,10 @@
 set -a fish_user_paths \
-    $HOME/.bun/bin/ \
-    $HOME/.cargo/bin/ \
-    $HOME/.go/bin/ \
-    $HOME/.krew/bin/ \
-    $HOME/.local/bin/
+    $CARGO_HOME/bin \
+    $GOPATH/bin \
+    $HOME/.bun/bin \
+    $HOME/.krew/bin \
+    $HOME/.local/bin
 
 if set -q WSL_DISTRO_NAME
-    set -a fish_user_paths "/mnt/c/Program Files/win32yank/"
+    set -a fish_user_paths "/mnt/c/Program Files/win32yank"
 end
