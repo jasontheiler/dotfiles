30145791294945e68e50f2d02662d5af3bc7c0f0
Jason Theiler <turret.jason@outlook.com>
2024-12-03 14:22:01 +0100

    feat: update tmux config

--- tmux/.config/fish/functions/tmux_hook_session_created.fish
+++ tmux/.config/fish/functions/tmux_hook_session_created.fish
@@ -1,9 +1,15 @@
 function tmux_hook_session_created
-    set window_1 (tmux display-message -p "#{window_id}")
+    set WINDOW_FORMAT "#{window_id}"
+    set PANE_FORMAT "#{pane_id}"
+
+    set session_name $argv[1]
+    set session \$$session_name
+
+    set window_1 (tmux display-message -t $session -p $WINDOW_FORMAT)
     tmux select-layout -t $window_1 main-vertical
 
-    set pane_1_1 (tmux display-message -t $window_1 -p "#{pane_id}")
+    set pane_1_1 (tmux display-message -t $window_1 -p $PANE_FORMAT)
     tmux send-keys -t $pane_1_1 " nvim" Enter
 
-    set window_2 (tmux new-window -dPF "#{window_id}")
+    set window_2 (tmux new-window -at $window_1 -dPF $WINDOW_FORMAT)
 end
