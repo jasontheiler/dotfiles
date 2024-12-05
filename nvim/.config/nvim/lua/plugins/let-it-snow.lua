22b97bf7bf811f2c45e970387ee0bc18815a4363
Jason Theiler <turret.jason@outlook.com>
2024-12-05 01:15:58 +0100

    feat: update Neovim config

---
+++ nvim/.config/nvim/lua/plugins/let-it-snow.lua
@@ -0,0 +1,7 @@
+-- See: https://github.com/marcussimonsen/let-it-snow.nvim
+return {
+  "marcussimonsen/let-it-snow.nvim",
+  event = "VeryLazy",
+  -- See: https://github.com/marcussimonsen/let-it-snow.nvim#configuration
+  opts = {},
+}
