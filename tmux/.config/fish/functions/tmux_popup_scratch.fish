dd844f1a64372f081688d17b11e6cc9ccd38eef0
Jason Theiler <turret.jason@outlook.com>
2024-12-25 04:01:59 +0100

    feat: update tmux config

---
+++ tmux/.config/fish/functions/tmux_popup_scratch.fish
@@ -0,0 +1,7 @@
+function tmux_popup_scratch
+    set client_name (tmux display-message -p "#{client_name}")
+    tmux new-session -A \
+        -c $HOME \
+        -e "TMUXION_TARGET_CLIENT=$client_name" \
+        -s "scratch_$client_name"
+end
