22bcdea40f0f3da3c6f42b45c32e052566f3c130
Jason Theiler <turret.jason@outlook.com>
2025-01-30 00:56:19 +0100

    feat: update fish config

--- fish/.config/fish/conf.d/$envs.fish
+++ fish/.config/fish/conf.d/$envs.fish
@@ -4,6 +4,15 @@ set -x DOTFILES_DIR (string replace /fish/.config/fish/conf.d/\$envs.fish "" $th
 set -x CARGO_HOME $HOME/.cargo
 set -x GOPATH $HOME/.go
 
+set -a fish_user_paths \
+    $CARGO_HOME/bin \
+    $GOPATH/bin \
+    $HOME/.bun/bin \
+    $HOME/.deno/bin \
+    $HOME/.krew/bin \
+    $HOME/.local/bin
+
 if set -q WSL_DISTRO_NAME
     set -x BROWSER explorer.exe
+    set -a fish_user_paths "/mnt/c/Program Files/win32yank"
 end
