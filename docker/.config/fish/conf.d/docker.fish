c4919df1792d30cec925d02b4cf8f5094752e47d
Jason Theiler <turret.jason@outlook.com>
2025-02-03 00:35:28 +0100

    feat: update Docker config

--- docker/.config/fish/conf.d/docker.fish
+++ docker/.config/fish/conf.d/docker.fish
@@ -5,7 +5,7 @@ abbr -a dcl docker compose logs -f
 abbr -a dcul docker compose up -d \&\& docker compose logs -f
 abbr -a dcubl docker compose up -d --build \&\& docker compose logs -f
 abbr -a dcd docker compose down
-abbr -a dps 'docker ps -a --format "table {{.ID}}\t{{printf \"%.24s\" .Names}}\t{{.RunningFor}}\t{{.Status}}"'
-abbr -a dpsv 'docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.RunningFor}}\t{{.Status}}"'
-abbr -a dpsd 'docker ps -a --format "table {{.ID}}\t{{printf \"%.24s\" .Names}}\t{{printf \"%.24s\" .Image}}\t{{printf \"%.24s\" .Ports}}\t{{printf \"%.24s\" .Networks}}\t{{.Size}}"'
-abbr -a dpsdv 'docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Ports}}\t{{.Networks}}\t{{.Size}}"'
+abbr -a dps 'docker ps -a --format="table {{.ID}}\t{{printf \"%.24s\" .Names}}\t{{.RunningFor}}\t{{.Status}}"'
+abbr -a dpsv 'docker ps -a --format="table {{.ID}}\t{{.Names}}\t{{.RunningFor}}\t{{.Status}}"'
+abbr -a dpsd 'docker ps -a --format="table {{.ID}}\t{{printf \"%.24s\" .Names}}\t{{printf \"%.24s\" .Image}}\t{{printf \"%.24s\" .Ports}}\t{{printf \"%.24s\" .Networks}}\t{{.Size}}"'
+abbr -a dpsdv 'docker ps -a --format="table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Ports}}\t{{.Networks}}\t{{.Size}}"'
