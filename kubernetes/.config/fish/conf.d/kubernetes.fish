c44ade6804fa7cfbf28b00be81737235abea0722
Jason Theiler <turret.jason@outlook.com>
2025-02-12 12:21:26 +0100

    feat: update Kubernetes config

--- kubernetes/.config/fish/conf.d/kubernetes.fish
+++ kubernetes/.config/fish/conf.d/kubernetes.fish
@@ -1,6 +1,8 @@
 abbr -a k kubectl
 abbr -a kg kubectl get
 abbr -a kd kubectl describe
+abbr -a kl kubectl logs
+abbr -a klf kubectl logs -f
 abbr -a kc kubectl create
 abbr -a kcf kubectl create -f
 abbr -a kck kubectl create -k
@@ -11,3 +13,4 @@ abbr -a kx kubectl config use-context
 abbr -a kxl kubectl config get-contexts
 abbr -a kxu kubectl config unset current-context
 abbr -a kns --set-cursor="<NAMESPACE>" kubectl config set-context --current --namespace="<NAMESPACE>"
+abbr -a knsl kubectl get ns
