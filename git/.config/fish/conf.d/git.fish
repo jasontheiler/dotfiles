956042bdcb34b4e2872625713af314b803bbb9b1
Jason Theiler <turret.jason@outlook.com>
2025-03-01 11:59:48 +0100

    feat: update Git config

--- git/.config/fish/conf.d/git.fish
+++ git/.config/fish/conf.d/git.fish
@@ -3,5 +3,6 @@ set -x GIT_TEMPLATE_DIR $DOTFILES_DIR/git/template
 abbr -a gc git commit
 abbr -a gcm --set-cursor="<MESSAGE>" git commit -m \""<MESSAGE>"\"
 abbr -a gp git push
+abbr -a gu git reset --mixed @~
 abbr -a gri --set-cursor="<COUNT>" git rebase -i @~"<COUNT>"
 abbr -a gra git rebase --abort
