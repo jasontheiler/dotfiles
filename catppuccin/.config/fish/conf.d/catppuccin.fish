12e717b4007fe675167ef0cba88d6e8653fe42dc
Jason Theiler <turret.jason@outlook.com>
2025-03-11 12:16:52 +0100

    feat: update fish config

--- catppuccin/.config/fish/conf.d/catppuccin.fish
+++ catppuccin/.config/fish/conf.d/catppuccin.fish
@@ -28,62 +28,62 @@
 #     CRUST "#11111B"
 
 # Catppuccin Mocha Warm
-# set -l colors \
-#     ROSEWATER "#DAA992" \
-#     FLAMINGO "#DAA992" \
-#     PINK "#C386BB" \
-#     MAUVE "#C386BB" \
-#     RED "#EA6962" \
-#     MAROON "#D3869B" \
-#     PEACH "#DD7F4E" \
-#     YELLOW "#E8C677" \
-#     GREEN "#A9B665" \
-#     TEAL "#89B482" \
-#     SKY "#89B482" \
-#     SAPPHIRE "#89B482" \
-#     BLUE "#7DAEB3" \
-#     LAVENDER "#BDAEC3" \
-#     TEXT "#E4CEA8" \
-#     SUBTEXT1 "#DCC6A0" \
-#     SUBTEXT0 "#D4BE98" \
-#     OVERLAY2 "#877360" \
-#     OVERLAY1 "#7D6C60" \
-#     OVERLAY0 "#75615B" \
-#     SURFACE2 "#615556" \
-#     SURFACE1 "#534747" \
-#     SURFACE0 "#433737" \
-#     BASE "#211D1D" \
-#     MANTLE "#191414" \
-#     CRUST "#110D0D"
+set -l colors \
+    ROSEWATER "#DAA992" \
+    FLAMINGO "#DAA992" \
+    PINK "#C386BB" \
+    MAUVE "#C386BB" \
+    RED "#EA6962" \
+    MAROON "#D3869B" \
+    PEACH "#DD7F4E" \
+    YELLOW "#E8C677" \
+    GREEN "#A9B665" \
+    TEAL "#89B482" \
+    SKY "#89B482" \
+    SAPPHIRE "#89B482" \
+    BLUE "#7DAEB3" \
+    LAVENDER "#BDAEC3" \
+    TEXT "#E4CEA8" \
+    SUBTEXT1 "#DCC6A0" \
+    SUBTEXT0 "#D4BE98" \
+    OVERLAY2 "#877360" \
+    OVERLAY1 "#7D6C60" \
+    OVERLAY0 "#75615B" \
+    SURFACE2 "#615556" \
+    SURFACE1 "#534747" \
+    SURFACE0 "#433737" \
+    BASE "#211D1D" \
+    MANTLE "#191414" \
+    CRUST "#110D0D"
 
 # Catppuccin Mocha Neo
-set -l colors \
-    ROSEWATER "#FFCDCD" \
-    FLAMINGO "#FFCDCD" \
-    PINK "#E3AAFF" \
-    MAUVE "#E3AAFF" \
-    RED "#FF87BC" \
-    MAROON "#F394B8" \
-    PEACH "#FFB7AB" \
-    YELLOW "#F9E6C0" \
-    GREEN "#B3F6C0" \
-    TEAL "#8CF8F7" \
-    SKY "#8CF8F7" \
-    SAPPHIRE "#8CF8F7" \
-    BLUE "#A6DBFF" \
-    LAVENDER "#C4BEFF" \
-    TEXT "#E0E2EA" \
-    SUBTEXT1 "#B9BCC7" \
-    SUBTEXT0 "#A5A8B5" \
-    OVERLAY2 "#9295A4" \
-    OVERLAY1 "#7E8192" \
-    OVERLAY0 "#6B6D81" \
-    SURFACE2 "#585A6F" \
-    SURFACE1 "#45475D" \
-    SURFACE0 "#32334B" \
-    BASE "#14161B" \
-    MANTLE "#0C0E13" \
-    CRUST "#04060B"
+# set -l colors \
+#     ROSEWATER "#FFCDCD" \
+#     FLAMINGO "#FFCDCD" \
+#     PINK "#E3AAFF" \
+#     MAUVE "#E3AAFF" \
+#     RED "#FF87BC" \
+#     MAROON "#F394B8" \
+#     PEACH "#FFB7AB" \
+#     YELLOW "#F9E6C0" \
+#     GREEN "#B3F6C0" \
+#     TEAL "#8CF8F7" \
+#     SKY "#8CF8F7" \
+#     SAPPHIRE "#8CF8F7" \
+#     BLUE "#A6DBFF" \
+#     LAVENDER "#C4BEFF" \
+#     TEXT "#E0E2EA" \
+#     SUBTEXT1 "#B9BCC7" \
+#     SUBTEXT0 "#A5A8B5" \
+#     OVERLAY2 "#9295A4" \
+#     OVERLAY1 "#7E8192" \
+#     OVERLAY0 "#6B6D81" \
+#     SURFACE2 "#585A6F" \
+#     SURFACE1 "#45475D" \
+#     SURFACE0 "#32334B" \
+#     BASE "#14161B" \
+#     MANTLE "#0C0E13" \
+#     CRUST "#04060B"
 
 for i in (seq 1 2 (count $colors))
     set -l k $colors[$i]
