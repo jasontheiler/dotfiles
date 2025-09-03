0709556ee7ee15a7d5be09a8ad76e5ff58f5ca05
Jason Theiler <jasontheiler@pm.me>
2025-09-04 01:22:06 +0200

    feat: update Neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,3 @@
--- Special thanks to TJ DeVries and ThePrimeagen for improving the Neovim
--- experience and helping to grow an awesome community around it.
-
 if vim.fn.has("nvim-0.11") == 0 then
   error("The loaded configuration requires Neovim >= 0.11")
 end
