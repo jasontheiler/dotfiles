434ecb2f69fc35fffa907c094d1feaf8f1cf3745
Jason Theiler <jasontheiler@pm.me>
2025-12-20 02:06:27 +0100

    feat: update Neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -454,82 +454,6 @@ vim.api.nvim_create_user_command("ToggleDiagnostics", function()
   end
 end, {})
 
--- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#clangd
-vim.lsp.config("clangd", { filetypes = { "c", "cpp", "objc", "objcpp", "cuda" } })
-
--- See:
---   - https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#denols
---   - https://docs.deno.com/runtime/getting_started/setup_your_environment/#neovim-0.6%2B-using-the-built-in-language-server
-vim.lsp.config("denols", {
-  root_dir = function(_, cb)
-    local cwd = vim.fn.getcwd()
-    for _, suffix in ipairs({ "/deno.json", "/deno.jsonc" }) do
-      if vim.uv.fs_stat(cwd .. suffix) then
-        cb(cwd)
-      end
-    end
-  end,
-})
-
--- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#html
-vim.lsp.config("html", { filetypes = { "html" } })
-
--- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
-vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      runtime = { version = "LuaJIT" },
-      workspace = {
-        checkThirdParty = false,
-        library = { vim.env.VIMRUNTIME },
-      },
-    },
-  },
-})
-
--- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#rust_analyzer
-vim.lsp.config("rust_analyzer", {
-  settings = {
-    ["rust-analyzer"] = {
-      cargo = { features = "all" },
-      check = { command = "clippy" },
-    },
-  },
-})
-
--- See:
---   - https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#ts_ls
---   - https://github.com/vuejs/language-tools#hybrid-mode-configuration-requires-vuelanguage-server-version-200
---   - https://docs.deno.com/runtime/getting_started/setup_your_environment/#neovim-0.6%2B-using-the-built-in-language-server
-vim.lsp.config("ts_ls", {
-  init_options = {
-    plugins = {
-      {
-        name = "@vue/typescript-plugin",
-        location = vim.fn.expand("$MASON/packages/vue-language-server/node_modules/@vue/language-server/"),
-        languages = { "vue" },
-      },
-    },
-  },
-  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
-  root_dir = function(_, cb)
-    local cwd = vim.fn.getcwd()
-    for _, suffix in ipairs({ "/package.json", "/tsconfig.json" }) do
-      if vim.uv.fs_stat(cwd .. suffix) then
-        cb(cwd)
-      end
-    end
-  end,
-  single_file_support = false,
-})
-
--- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#yamlls
-vim.lsp.config("yamlls", {
-  settings = {
-    yaml = { keyOrdering = false },
-  },
-})
-
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>")
 vim.keymap.set({ "n", "v" }, "<Leader>p", "\"_dP", { desc = "Paste (without yank)" })
 
@@ -626,4 +550,80 @@ vim.api.nvim_create_autocmd("LspAttach", {
       desc = "Document symbols",
     })
   end,
+})
+
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#clangd
+vim.lsp.config("clangd", { filetypes = { "c", "cpp", "objc", "objcpp", "cuda" } })
+
+-- See:
+--   - https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#denols
+--   - https://docs.deno.com/runtime/getting_started/setup_your_environment/#neovim-0.6%2B-using-the-built-in-language-server
+vim.lsp.config("denols", {
+  root_dir = function(_, cb)
+    local cwd = vim.fn.getcwd()
+    for _, suffix in ipairs({ "/deno.json", "/deno.jsonc" }) do
+      if vim.uv.fs_stat(cwd .. suffix) then
+        cb(cwd)
+      end
+    end
+  end,
+})
+
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#html
+vim.lsp.config("html", { filetypes = { "html" } })
+
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      workspace = {
+        checkThirdParty = false,
+        library = { vim.env.VIMRUNTIME },
+      },
+    },
+  },
+})
+
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#rust_analyzer
+vim.lsp.config("rust_analyzer", {
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = { features = "all" },
+      check = { command = "clippy" },
+    },
+  },
+})
+
+-- See:
+--   - https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#ts_ls
+--   - https://github.com/vuejs/language-tools#hybrid-mode-configuration-requires-vuelanguage-server-version-200
+--   - https://docs.deno.com/runtime/getting_started/setup_your_environment/#neovim-0.6%2B-using-the-built-in-language-server
+vim.lsp.config("ts_ls", {
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vim.fn.expand("$MASON/packages/vue-language-server/node_modules/@vue/language-server/"),
+        languages = { "vue" },
+      },
+    },
+  },
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+  root_dir = function(_, cb)
+    local cwd = vim.fn.getcwd()
+    for _, suffix in ipairs({ "/package.json", "/tsconfig.json" }) do
+      if vim.uv.fs_stat(cwd .. suffix) then
+        cb(cwd)
+      end
+    end
+  end,
+  single_file_support = false,
+})
+
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#yamlls
+vim.lsp.config("yamlls", {
+  settings = {
+    yaml = { keyOrdering = false },
+  },
 })
