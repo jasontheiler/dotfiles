473ec9fa3c2f187228e55fd3feeca6871b594279
Jason Theiler <jasontheiler@pm.me>
2026-01-16 16:57:16 +0100

    feat: update fish config

--- fish/.config/fish/conf.d/$envs.fish
+++ fish/.config/fish/conf.d/$envs.fish
@@ -1,6 +1,8 @@
 set -l this_file_path (path resolve (status current-filename))
 set -x DOTFILES_DIR (string replace /fish/.config/fish/conf.d/\$envs.fish "" $this_file_path)
 
+set -x PAGER "less -RS"
+
 set -x CARGO_HOME $HOME/.cargo
 set -x GOPATH $HOME/.go
 
